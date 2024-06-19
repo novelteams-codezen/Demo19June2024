d7b8bc19683217953e007b920f7b32c008f6758c
Novel Teams Codezen <159769018+novelteams-codezen@users.noreply.github.com>
2024-06-19 15:22:12 +0200

    Upload files from codezen

---
+++ API/Models/AppSetting.g.cs
@@ -0,0 +1,9 @@
+namespace Demo19June2024.Models
+{
+    public static class AppSetting
+    {
+        public static string? JwtKey { get; set; }
+        public static string? JwtIssuer { get; set; }
+        public static int TokenExpirationtime { get; set; }
+    }
+}
